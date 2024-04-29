e7ca1b09b37cf1a41e20bb5897fd072af4426ab1
gustavo <gustavo.isaac@gmail.com>
2024-04-29 21:13:35 +0100

    PokemonMapper

---
+++ app/src/main/java/com/artemissoftware/pokeconnect/core/data/mappers/PokemonMapper.kt
@@ -0,0 +1,26 @@
+package com.artemissoftware.pokeconnect.core.data.mappers
+
+import com.artemissoftware.pokeconnect.core.models.PokedexEntry
+import com.artemissoftware.pokeconnect.core.network.PokeApi
+import com.artemissoftware.pokeconnect.core.network.dto.PokedexEntryDto
+
+fun PokedexEntryDto.toPokedexEntry(): PokedexEntry{
+    val id = url.toPokemonId()
+    return PokedexEntry(
+        id = id.toInt(),
+        name = name,
+        imageUrl = id.getImageUrl()
+    )
+}
+
+private fun String.toPokemonId(): String {
+    return if(this.endsWith("/")) {
+        this.dropLast(1).takeLastWhile { it.isDigit() }
+    } else {
+        this.takeLastWhile { it.isDigit() }
+    }
+}
+
+private fun String.getImageUrl(): String {
+    return PokeApi.DEFAULT_IMAGE_URL + this + PokeApi.DEFAULT_IMAGE_URL_EXTENSION
+}
