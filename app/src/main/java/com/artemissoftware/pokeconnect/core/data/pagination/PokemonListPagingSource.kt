d459036915a5a1b82faf8ba345448a2d52ccbcf1
gustavo <gustavo.isaac@gmail.com>
2024-04-29 20:44:25 +0100

    PokemonListPagingSource

---
+++ app/src/main/java/com/artemissoftware/pokeconnect/core/data/pagination/PokemonListPagingSource.kt
@@ -0,0 +1,58 @@
+package com.artemissoftware.pokeconnect.core.data.pagination
+
+import androidx.paging.PagingSource
+import androidx.paging.PagingState
+import com.artemissoftware.pokeconnect.core.data.HandleNetwork
+import com.artemissoftware.pokeconnect.core.domain.Resource
+import com.artemissoftware.pokeconnect.core.domain.error.DataError
+import com.artemissoftware.pokeconnect.core.domain.exceptions.PaginationException
+import com.artemissoftware.pokeconnect.core.network.PokeApi
+import com.artemissoftware.pokeconnect.core.network.dto.PokemonLisDto
+import com.artemissoftware.pokeconnect.core.network.dto.ResumeDto
+import com.artemissoftware.pokeconnect.core.network.source.PokeApiSource
+
+class PokemonListPagingSource(
+    private val pokeApiSource: PokeApiSource,
+) : PagingSource<Int, ResumeDto>() {
+
+    private var totalCount = 0
+
+    override fun getRefreshKey(state: PagingState<Int, ResumeDto>): Int? {
+        return state.anchorPosition
+    }
+
+    override suspend fun load(params: LoadParams<Int>): LoadResult<Int, ResumeDto> {
+        val currentPage = params.key ?: 1
+
+        val result = HandleNetwork.safeNetworkCall {
+            pokeApiSource.getPokemonList(limit = PokeApi.PAGE_SIZE, offset = currentPage * PokeApi.PAGE_SIZE)
+        }
+
+        return when (result) {
+            is Resource.Failure -> {
+                LoadResult.Error(throwable = PaginationException(result.error as DataError.NetworkError))
+            }
+            is Resource.Success -> {
+                val pokemonList = result.data
+                totalCount += pokemonList.results.size
+
+                var data = emptyList<ResumeDto>()
+                var prevKey: Int? = null
+                var nextKey: Int? = null
+
+                if (totalCount <= pokemonList.count) {
+                    val endOfPaginationReached =  currentPage * PokeApi.PAGE_SIZE >= pokemonList.count
+                    data = pokemonList.results
+                    prevKey = if (currentPage == 1) null else currentPage - 1
+                    nextKey = if (endOfPaginationReached) null else currentPage + 1
+                }
+
+                LoadResult.Page(
+                    data = data,
+                    prevKey = prevKey,
+                    nextKey = nextKey,
+                )
+            }
+        }
+    }
+}
