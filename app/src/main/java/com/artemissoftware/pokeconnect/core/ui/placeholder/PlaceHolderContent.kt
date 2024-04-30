9921061673c5c4e12fbfcb01c61f8c9230319a14
gustavo <gustavo.isaac@gmail.com>
2024-04-30 18:37:26 +0100

    PlaceHolderContent

---
+++ app/src/main/java/com/artemissoftware/pokeconnect/core/ui/placeholder/PlaceHolderContent.kt
@@ -0,0 +1,81 @@
+package com.artemissoftware.pokeconnect.core.ui.placeholder
+
+import androidx.annotation.DrawableRes
+import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.animation.core.tween
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.res.painterResource
+import com.artemissoftware.pokeconnect.R
+import com.artemissoftware.pokeconnect.core.designsystem.PokeConnectTheme
+import com.artemissoftware.pokeconnect.core.designsystem.ThemePreviews
+import com.artemissoftware.pokeconnect.core.designsystem.dimension
+import com.artemissoftware.pokeconnect.core.designsystem.spacing
+
+
+@Composable
+fun PlaceHolderContent(
+    message: String,
+    @DrawableRes icon: Int = R.drawable.ic_placeholder,
+) {
+    var startAnimation by remember {
+        mutableStateOf(false)
+    }
+
+    val alphaAnimation by animateFloatAsState(
+        targetValue = if (startAnimation) 0.8f else 0f,
+        animationSpec = tween(durationMillis = 1000),
+        label = "",
+    )
+
+    LaunchedEffect(key1 = true) {
+        startAnimation = true
+    }
+
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center,
+    ) {
+        Icon(
+            painter = painterResource(id = icon),
+            contentDescription = null,
+            modifier = Modifier
+                .size(MaterialTheme.dimension.iconSizeBig)
+                .alpha(alphaAnimation),
+        )
+        Text(
+            modifier = Modifier
+                .padding(MaterialTheme.spacing.spacing1_5)
+                .alpha(alphaAnimation),
+            text = message,
+            style = MaterialTheme.typography.bodyMedium,
+        )
+    }
+}
+
+
+@ThemePreviews
+@Composable
+private fun PlaceHolderContentPreview() {
+    PokeConnectTheme {
+        PlaceHolderContent(
+            message = "Internet Unavailable.",
+        )
+    }
+}
