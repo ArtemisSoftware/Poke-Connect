cac04b4d1144d3f3bdcd79b41b9d4c39d699edc1
gustavo <gustavo.isaac@gmail.com>
2024-04-30 14:30:16 +0100

    ErrorExtensions

---
+++ app/src/main/java/com/artemissoftware/pokeconnect/core/presentation/util/extensions/ErrorExtensions.kt
@@ -0,0 +1,34 @@
+package com.artemissoftware.pokeconnect.core.presentation.util.extensions
+
+import com.artemissoftware.pokeconnect.R
+import com.artemissoftware.pokeconnect.core.domain.error.DataError
+import com.artemissoftware.pokeconnect.core.domain.error.Error
+import com.artemissoftware.pokeconnect.core.ui.text.UiText
+
+
+fun Error.toUiText(): UiText {
+    return when (this) {
+        is DataError.NetworkError -> {
+            this.asUiText()
+        }
+        else -> UiText.StringResource(R.string.error_not_mapped)
+    }
+}
+
+private fun DataError.NetworkError.asUiText(): UiText {
+    return when (this) {
+        DataError.NetworkError.Connect -> UiText.StringResource(
+            R.string.connection_error,
+        )
+        is DataError.NetworkError.Error -> UiText.DynamicString(this.message)
+        DataError.NetworkError.SocketTimeout -> UiText.StringResource(
+            R.string.connection_socket_time_out,
+        )
+        DataError.NetworkError.Unknown -> UiText.StringResource(
+            R.string.unknown_error,
+        )
+        DataError.NetworkError.UnknownHost -> UiText.StringResource(
+            R.string.unknown_host_error,
+        )
+    }
+}
