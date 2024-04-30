39be8e6cd821a16688969372f4e90eb390029235
gustavo <gustavo.isaac@gmail.com>
2024-04-30 11:28:54 +0100

    ThemePreviews

---
+++ app/src/main/java/com/artemissoftware/pokeconnect/core/designsystem/ThemePreviews.kt
@@ -0,0 +1,11 @@
+package com.artemissoftware.pokeconnect.core.designsystem
+
+import androidx.compose.ui.tooling.preview.Devices
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.tooling.preview.PreviewLightDark
+import androidx.compose.ui.tooling.preview.PreviewScreenSizes
+
+@Preview(name = "Phone - Landscape",
+    device = "spec:width = 411dp, height = 891dp, orientation = landscape, dpi = 420")
+@PreviewLightDark
+annotation class ThemePreviews
