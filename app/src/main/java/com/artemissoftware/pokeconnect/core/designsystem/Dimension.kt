c7a059241269ea38529c789ae571a256b53827f0
gustavo <gustavo.isaac@gmail.com>
2024-04-30 10:40:05 +0100

    Dimension design

---
+++ app/src/main/java/com/artemissoftware/pokeconnect/core/designsystem/Dimension.kt
@@ -0,0 +1,32 @@
+package com.artemissoftware.pokeconnect.core.designsystem
+
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.staticCompositionLocalOf
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.DpSize
+import androidx.compose.ui.unit.dp
+
+data class Dimension(
+    val cardImage: DpSize,
+    val iconChipSize: DpSize,
+    val iconSize: DpSize,
+    val iconSizeBig: DpSize,
+    val iconContentSize: DpSize,
+    val statMaxWidth: Dp,
+)
+
+val dimension = Dimension(
+    cardImage = DpSize(width = 120.dp, height = 120.dp),
+    iconSize = DpSize(width = 46.dp, height = 46.dp),
+    iconSizeBig = DpSize(width = 120.dp, height = 120.dp),
+    iconChipSize = DpSize(width = 18.dp, height = 18.dp),
+    iconContentSize = DpSize(width = 20.dp, height = 20.dp),
+    statMaxWidth = 48.dp
+)
+
+internal val localDimension = staticCompositionLocalOf<Dimension> { throw IllegalStateException("No theme installed") }
+
+val MaterialTheme.dimension: Dimension
+    @Composable
+    get() = localDimension.current
