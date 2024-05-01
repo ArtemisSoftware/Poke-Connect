8d8401e3450cca8a7c6ed971b6b000860e753abe
gustavo <gustavo.isaac@gmail.com>
2024-05-01 10:24:02 +0100

    StatEntity

---
+++ app/src/main/java/com/artemissoftware/pokeconnect/core/database/entities/StatEntity.kt
@@ -0,0 +1,21 @@
+package com.artemissoftware.pokeconnect.core.database.entities
+
+import androidx.room.Entity
+import androidx.room.ForeignKey
+
+@Entity(
+    primaryKeys = ["pokemonId", "description"],
+    foreignKeys = [
+        ForeignKey(
+            entity = PokemonEntity::class,
+            parentColumns = ["id"],
+            childColumns = ["pokemonId"],
+            onDelete = ForeignKey.CASCADE // If the parent is deleted, child records are deleted too
+        )
+    ]
+)
+data class StatEntity(
+    val pokemonId: Int,
+    val description: String,
+    val value: Int
+)
