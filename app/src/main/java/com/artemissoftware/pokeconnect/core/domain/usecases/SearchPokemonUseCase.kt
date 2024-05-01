d195ad8023cceea612129478d41eb7a45e37c90d
gustavo <gustavo.isaac@gmail.com>
2024-05-01 10:04:27 +0100

    SearchPokemonUseCase

---
+++ app/src/main/java/com/artemissoftware/pokeconnect/core/domain/usecases/SearchPokemonUseCase.kt
@@ -0,0 +1,8 @@
+package com.artemissoftware.pokeconnect.core.domain.usecases
+
+import com.artemissoftware.pokeconnect.core.domain.repositories.PokemonRepository
+import javax.inject.Inject
+
+class SearchPokemonUseCase @Inject constructor(private val pokemonRepository: PokemonRepository) {
+    suspend operator fun invoke(query: String) = pokemonRepository.getPokemon(query = query)
+}
