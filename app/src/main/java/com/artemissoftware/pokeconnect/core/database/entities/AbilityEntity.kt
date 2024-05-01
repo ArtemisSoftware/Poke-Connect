93c7e87341fc9a6caf483099ad5514bf6b66783e
gustavo <gustavo.isaac@gmail.com>
2024-05-01 10:23:40 +0100

    AbilityEntity

---
+++ app/src/main/java/com/artemissoftware/pokeconnect/core/database/entities/AbilityEntity.kt
@@ -0,0 +1,20 @@
+package com.artemissoftware.pokeconnect.core.database.entities
+
+import androidx.room.Entity
+import androidx.room.ForeignKey
+
+@Entity(
+    primaryKeys = ["pokemonId", "description"],
+    foreignKeys = [
+        ForeignKey(
+            entity = PokemonEntity::class,
+            parentColumns = ["id"],
+            childColumns = ["pokemonId"],
+            onDelete = ForeignKey.CASCADE // If the parent is deleted, child records are deleted too
+        )
+    ]
+)
+data class AbilityEntity(
+    val pokemonId: Int,
+    val description: String,
+)
