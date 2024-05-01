df0d144de80ee4b90dd16af39b391afb2dac7127
gustavo <gustavo.isaac@gmail.com>
2024-05-01 09:12:51 +0100

    Pokemon

---
+++ app/src/main/java/com/artemissoftware/pokeconnect/core/models/Pokemon.kt
@@ -0,0 +1,11 @@
+package com.artemissoftware.pokeconnect.core.models
+
+data class Pokemon(
+    val id: Int,
+    val name: String,
+    val height: Int,
+    val weight: Int,
+    val imageUrl: String,
+    val stats: List<Stat> = emptyList(),
+    val abilities: List<String> = emptyList()
+)
