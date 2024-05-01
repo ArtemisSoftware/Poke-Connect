e39b4aba9e653747658c76a9abcf867cd7105698
gustavo <gustavo.isaac@gmail.com>
2024-05-01 10:48:24 +0100

    PokemonDataBase

---
+++ app/src/main/java/com/artemissoftware/pokeconnect/core/database/PokemonDataBase.kt
@@ -0,0 +1,22 @@
+package com.artemissoftware.pokeconnect.core.database
+
+import androidx.room.Database
+import androidx.room.RoomDatabase
+import com.artemissoftware.pokeconnect.core.database.dao.PokemonDao
+import com.artemissoftware.pokeconnect.core.database.entities.AbilityEntity
+import com.artemissoftware.pokeconnect.core.database.entities.PokemonEntity
+import com.artemissoftware.pokeconnect.core.database.entities.StatEntity
+
+@Database(
+    entities = [
+        PokemonEntity::class,
+        StatEntity::class,
+        AbilityEntity::class,
+    ],
+    version = 1,
+    exportSchema = false,
+)
+abstract class PokemonDataBase : RoomDatabase() {
+
+    abstract fun getPokemonDao(): PokemonDao
+}
