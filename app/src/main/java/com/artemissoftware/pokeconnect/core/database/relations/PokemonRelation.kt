3e16505fa1e5c093ab9ed4be02a2de7356ad11e7
gustavo <gustavo.isaac@gmail.com>
2024-05-01 10:26:54 +0100

    PokemonRelation

---
+++ app/src/main/java/com/artemissoftware/pokeconnect/core/database/relations/PokemonRelation.kt
@@ -0,0 +1,21 @@
+package com.artemissoftware.pokeconnect.core.database.relations
+
+import androidx.room.Embedded
+import androidx.room.Relation
+import com.artemissoftware.pokeconnect.core.database.entities.AbilityEntity
+import com.artemissoftware.pokeconnect.core.database.entities.PokemonEntity
+import com.artemissoftware.pokeconnect.core.database.entities.StatEntity
+
+data class PokemonRelation(
+    @Embedded val pokemon: PokemonEntity,
+    @Relation(
+        parentColumn = "id",
+        entityColumn = "pokemonId"
+    )
+    val stats: List<StatEntity>,
+    @Relation(
+        parentColumn = "id",
+        entityColumn = "pokemonId"
+        )
+    val abilities: List<AbilityEntity>
+)
