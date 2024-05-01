83a7ab8b907c3ebfe5627732a57122fdaf54df72
gustavo <gustavo.isaac@gmail.com>
2024-05-01 10:46:32 +0100

    PokemonDao

---
+++ app/src/main/java/com/artemissoftware/pokeconnect/core/database/dao/PokemonDao.kt
@@ -0,0 +1,37 @@
+package com.artemissoftware.pokeconnect.core.database.dao
+
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import androidx.room.Transaction
+import com.artemissoftware.pokeconnect.core.database.entities.AbilityEntity
+import com.artemissoftware.pokeconnect.core.database.entities.PokemonEntity
+import com.artemissoftware.pokeconnect.core.database.entities.StatEntity
+import com.artemissoftware.pokeconnect.core.database.relations.PokemonRelation
+
+@Dao
+interface PokemonDao {
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insert(pokemonEntity: PokemonEntity)
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insert(abilityEntities: List<AbilityEntity>)
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insert(statsEntities: List<StatEntity>)
+
+    @Transaction
+    @Query("SELECT * FROM PokemonEntity WHERE id = :id")
+    suspend fun getPokemon(id: Int): PokemonRelation
+
+    @Transaction
+    suspend fun insert(pokemonEntity: PokemonEntity, statsEntities: List<StatEntity>, abilityEntities: List<AbilityEntity>) {
+        insert(pokemonEntity = pokemonEntity)
+        insert(statsEntities = statsEntities)
+        insert(abilityEntities = abilityEntities)
+    }
+
+    @Query("DELETE FROM PokemonEntity WHERE id = :id")
+    suspend fun delete(id: Int)
+}
