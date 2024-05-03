205360255b2c4cd196a3c8af05753b106f82321f
gustavo <gustavo.isaac@gmail.com>
2024-05-03 16:07:46 +0100

    StringExtensions

--- app/src/main/java/com/artemissoftware/pokeconnect/core/common/util/extensions/StringExtensions.kt
+++ app/src/main/java/com/artemissoftware/pokeconnect/core/common/util/extensions/StringExtensions.kt
@@ -1,5 +1,3 @@
 package com.artemissoftware.pokeconnect.core.common.util.extensions
 
-import java.util.Locale
-
-fun String.upperCaseFirstChar() = this.replaceFirstChar { if (it.isLowerCase()) it.titlecase(Locale.getDefault()) else it.toString() }
+fun String.upperCaseFirstChar() = this.replaceFirstChar { if (it.isLowerCase()) it.titlecase() else it.toString() }
---
+++ app/src/test/java/com/artemissoftware/pokeconnect/common/util/extensions/StringExtensionsTest.kt
@@ -0,0 +1,26 @@
+package com.artemissoftware.pokeconnect.common.util.extensions
+
+import assertk.assertThat
+import assertk.assertions.isEqualTo
+import com.artemissoftware.pokeconnect.core.common.util.extensions.upperCaseFirstChar
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.Arguments
+import org.junit.jupiter.params.provider.MethodSource
+
+class StringExtensionsTest {
+
+    @ParameterizedTest
+    @MethodSource("numberFormats")
+    fun `Given word should return the word with first char in uppercase`(input: String, expected: String) {
+        assertThat(input.upperCaseFirstChar()).isEqualTo(expected)
+    }
+
+    private companion object {
+        @JvmStatic
+        fun numberFormats() = listOf(
+            Arguments.of("bulbasur", "Bulbasur"),
+            Arguments.of("charmander", "Charmander"),
+            Arguments.of("Pikachu", "Pikachu"),
+        )
+    }
+}
