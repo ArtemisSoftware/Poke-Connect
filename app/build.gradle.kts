a99ae102317767a14a37704f30c9b6a229f5facd
gustavo <gustavo.isaac@gmail.com>
2024-05-03 17:59:02 +0100

    hilt-android-testing and core-testing dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -99,6 +99,10 @@ dependencies {
     androidTestImplementation(platform(libs.androidx.compose.bom))
     androidTestImplementation(libs.androidx.ui.test.junit4)
     androidTestImplementation(libs.assertk)
+    androidTestImplementation(libs.core.testing)
+    androidTestImplementation(libs.hilt.android.testing)
+
+    kspAndroidTest(libs.hilt.android.compiler)
 
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
