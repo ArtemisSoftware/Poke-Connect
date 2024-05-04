a4974109cd275625876bab0495e7dbb13d71fd53
gustavo <gustavo.isaac@gmail.com>
2024-05-04 03:04:39 +0100

    PokemonType

---
+++ app/src/main/java/com/artemissoftware/pokeconnect/core/models/PokemonType.kt
@@ -0,0 +1,28 @@
+package com.artemissoftware.pokeconnect.core.models
+
+enum class PokemonType(val description: String) {
+
+    NORMAL("normal"),
+    FIRE("fire"),
+    WATER("water"),
+    ELECTRIC("electric"),
+    GRASS("grass"),
+    ICE("ice"),
+    FIGHTING("fighting"),
+    POISON("poison"),
+    GROUND("ground"),
+    FLYING("flying"),
+    PSYCHIC("psychic"),
+    BUG("bug"),
+    ROCK("rock"),
+    GHOST("ghost"),
+    DRAGON("dragon"),
+    DARK("dark"),
+    STEEL("steel"),
+    FAIRY("fairy"),
+    UNKNOWN("Not typified");
+
+    companion object{
+        fun getType(description: String) = entries.find { it.description == description.lowercase() } ?: UNKNOWN
+    }
+}
