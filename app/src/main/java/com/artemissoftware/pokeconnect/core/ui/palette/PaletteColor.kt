fe728c8ee185911764481a84bf897ce5b4f7ff89
gustavo <gustavo.isaac@gmail.com>
2024-05-04 02:35:24 +0100

    PaletteColor

---
+++ app/src/main/java/com/artemissoftware/pokeconnect/core/ui/palette/PaletteColor.kt
@@ -0,0 +1,8 @@
+package com.artemissoftware.pokeconnect.core.ui.palette
+
+import androidx.compose.ui.graphics.Color
+
+data class PaletteColor(
+    val text: Color = Color.Black,
+    val background: Color = Color.Transparent,
+)
