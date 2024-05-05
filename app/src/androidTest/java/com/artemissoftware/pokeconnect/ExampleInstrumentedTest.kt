cc63b15cd3e81816524798c363f85920c9e1adcd
gustavo <gustavo.isaac@gmail.com>
2024-05-05 20:26:13 +0100

    missing files

--- app/src/androidTest/java/com/artemissoftware/pokeconnect/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/artemissoftware/pokeconnect/ExampleInstrumentedTest.kt
@@ -1,24 +1,66 @@
 package com.artemissoftware.pokeconnect
 
-import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.ext.junit.runners.AndroidJUnit4
-
+import assertk.assertThat
+import assertk.assertions.isEqualTo
+import com.artemissoftware.pokeconnect.core.database.PokemonDataBase
+import dagger.hilt.android.testing.HiltAndroidRule
+import dagger.hilt.android.testing.HiltAndroidTest
+import kotlinx.coroutines.test.runTest
+import org.junit.After
+import org.junit.Assert
+import org.junit.Before
+import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
-
-import org.junit.Assert.*
+import javax.inject.Inject
 
 /**
  * Instrumented test, which will execute on an Android device.
  *
  * See [testing documentation](http://d.android.com/tools/testing).
  */
+@HiltAndroidTest
 @RunWith(AndroidJUnit4::class)
 class ExampleInstrumentedTest {
-    @Test
-    fun useAppContext() {
-        // Context of the app under test.
-        val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.artemissoftware.pokeconnect", appContext.packageName)
+
+    @get:Rule
+    val hiltRule = HiltAndroidRule(this)
+
+    @Inject
+    lateinit var database: PokemonDataBase
+
+    @Before
+    open fun setUp() {
+        hiltRule.inject()
+        database.clearAllTables()
     }
+
+    @After
+    open fun tearDown() {
+        database.close()
+    }
+
+    @Test
+    fun useAppContext() = runTest {
+        database.getPokemonDao().insert(
+            pokemonEntity = TestInstrumentedData.pokemonEntity,
+            statsEntities = listOf(TestInstrumentedData.statEntry),
+            abilityEntities = listOf(TestInstrumentedData.abilityEntity),
+            typesEntities = listOf(TestInstrumentedData.typesEntity),
+        )
+        val result = database.getPokemonDao().getPokemon(TestInstrumentedData.pokemonEntity.id, TestInstrumentedData.pokemonEntity.name)
+
+        Assert.assertEquals(1, result.size)
+        assertThat(result[0]).isEqualTo(TestInstrumentedData.pokemonRelation)
+        //println(database.getPokemonDao())
+        //assert(true)
+    }
+
+//    @Test
+//    fun useAppContext() {
+//        // Context of the app under test.
+//        val appContext = InstrumentationRegistry.getInstrumentation().targetContext
+//        assertEquals("com.artemissoftware.pokeconnect", appContext.packageName)
+//    }
 }
--- app/src/main/java/com/artemissoftware/pokeconnect/MainActivity.kt
+++ app/src/main/java/com/artemissoftware/pokeconnect/MainActivity.kt
@@ -2,18 +2,27 @@ package com.artemissoftware.pokeconnect
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import androidx.activity.SystemBarStyle
 import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.artemissoftware.pokeconnect.ui.theme.PokeConnectTheme
+import androidx.navigation.compose.rememberNavController
+import com.artemissoftware.pokeconnect.core.designsystem.PokeConnectTheme
+import com.artemissoftware.pokeconnect.features.navigation.HOME_GRAPH
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        enableEdgeToEdge(
+            statusBarStyle = SystemBarStyle.light(
+                android.graphics.Color.TRANSPARENT,
+                android.graphics.Color.TRANSPARENT,
+            ),
+        )
         super.onCreate(savedInstanceState)
         setContent {
             PokeConnectTheme {
@@ -22,25 +31,14 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    val navController = rememberNavController()
+
+                    RootNavGraph(
+                        navController = navController,
+                        startDestination = HOME_GRAPH,
+                    )
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    PokeConnectTheme {
-        Greeting("Android")
     }
 }
