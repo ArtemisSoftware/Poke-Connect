b0614f77292382a4f05b990d7be04474323af96e
gustavo <gustavo.isaac@gmail.com>
2024-05-05 12:09:33 +0100

    UpdateFavoriteUseCaseTest

---
+++ app/src/test/java/com/artemissoftware/pokeconnect/domain/usecases/UpdateFavoriteUseCaseTest.kt
@@ -0,0 +1,40 @@
+package com.artemissoftware.pokeconnect.domain.usecases
+
+import assertk.assertThat
+import assertk.assertions.isEmpty
+import assertk.assertions.isNotEmpty
+import com.artemissoftware.pokeconnect.TestMockData
+import com.artemissoftware.pokeconnect.core.domain.usecases.UpdateFavoriteUseCase
+import com.artemissoftware.pokeconnect.data.fakes.FakeFavoritesRepository
+import kotlinx.coroutines.test.runTest
+import org.junit.jupiter.api.BeforeEach
+
+class UpdateFavoriteUseCaseTest {
+
+    private lateinit var favoritesRepository: FakeFavoritesRepository
+
+    private lateinit var updateFavoriteUseCase: UpdateFavoriteUseCase
+
+    @BeforeEach
+    fun setup() {
+        favoritesRepository = FakeFavoritesRepository()
+        updateFavoriteUseCase = UpdateFavoriteUseCase(favoritesRepository = favoritesRepository)
+    }
+
+    @org.junit.jupiter.api.Test
+    fun `save a pokemon that is not a favorite`() = runTest {
+
+        updateFavoriteUseCase(TestMockData.getPokemon(isFavorite = false))
+
+        assertThat(favoritesRepository.pokemonList).isNotEmpty()
+    }
+
+    @org.junit.jupiter.api.Test
+    fun `delete a pokemon that is a favorite`() = runTest {
+
+        updateFavoriteUseCase(TestMockData.getPokemon(isFavorite = false))
+        updateFavoriteUseCase(TestMockData.getPokemon(isFavorite = true))
+
+        assertThat(favoritesRepository.pokemonList).isEmpty()
+    }
+}
