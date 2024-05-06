4e0f8f684a9e8abacc0881bf1834eb2494cb1cc6
gustavo <gustavo.isaac@gmail.com>
2024-05-06 08:22:25 +0100

    DateTypeConverter

---
+++ app/src/main/java/com/artemissoftware/pokeconnect/core/database/converters/DateTypeConverter.kt
@@ -0,0 +1,16 @@
+package com.artemissoftware.pokeconnect.core.database.converters
+
+import androidx.room.TypeConverter
+import java.util.Date
+
+class DateTypeConverter {
+    @TypeConverter
+    fun fromTimestamp(value: Long): Date {
+        return Date(value)
+    }
+
+    @TypeConverter
+    fun dateToTimestamp(date: Date): Long {
+        return date.time
+    }
+}
